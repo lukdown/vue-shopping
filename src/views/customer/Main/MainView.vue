60c6ba81a325ba44d1a88dea7455260b2cb9e3e7
seed-yoo <lukdown10@naver.com>
2024-04-15 19:06:27 +0900

    메인슬라이드

--- src/views/customer/Main/MainView.vue
+++ src/views/customer/Main/MainView.vue
@@ -12,7 +12,24 @@
 
 
 
-              test
+              <vueper-slides autoplay>
+                <vueper-slide v-for="(slide, i) in slides"
+                  :key="i"
+                  :title="slide.title"
+                  :content="slide.content"
+                  :image="slide.image" />
+                  
+                <template #pause>
+                  <i class="icon pause_circle_outline"></i>
+                </template>
+              </vueper-slides>
+
+
+
+
+
+
+
 
 
 
@@ -37,18 +54,42 @@
 import "@/assets/css/customer/MainView.css";
 import AppFooter from "@/components/customer/AppFooter.vue";
 import AppHeader from "@/components/customer/AppHeader.vue";
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
 
 export default {
   name: "MainView",
   components: {
     AppHeader,
     AppFooter,
+    VueperSlides,
+    VueperSlide,
   },
   data() {
-    return {};
+    return {
+      pauseOnHover: true,
+      autoPlaying: true,
+      internalAutoPlaying: true,
+      slides: [
+        {
+          title: 'Slide <b style="font-size: 1.3em;color: yellow">#1</b>',
+          content: 'Slide title can be HTML.<br>And so does the slide content, <span style="font-size: 1.2em;color: yellow">why not?</span>',
+          image: require('@/assets/img/logo.png')
+        },
+        {
+          title: 'rerewrwerwet',
+          content: 'ytdryr',
+          image: require('@/assets/img/logo.png')
+        },
+        // Other slides...
+      ]
+    };
   },
-  methods: {},
+  methods: {
+    
+  },
   created() {},
 };
 </script>
+
 <style></style>
