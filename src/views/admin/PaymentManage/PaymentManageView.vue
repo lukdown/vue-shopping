3783112ca6f1cfe02bb41cba3b5a08ca8d499151
poula9744 <poula9744@naver.com>
2024-04-15 22:03:12 +0900

    이은빈

--- src/views/admin/PaymentManage/PaymentManageView.vue
+++ src/views/admin/PaymentManage/PaymentManageView.vue
@@ -8,16 +8,22 @@
         <!-- content-head 없음 -->
 
         <div id="index">
-            <div id="greetings">
+            <div id="greetings" class="m-payment-all">
                 <h1>결제관리</h1>
 
                 <!--카테고리별 정렬-->
-
-                <!--검색-->
-                <form id="m-payment-search-form" action="" method="">
-                    <input type="text" id="search" name="psearch" value="" placeholder="검색어를 입력하세요">
-                    <button>검색</button>
-                </form>
+                <div class="m-payment-btn-box">
+                    <div class="item">
+                        <select name="search-type" id="search-type" class="input-search">
+                            <option value="0">전체</option>
+                            <option value="1">구매자명</option>
+                            <option value="2">핸드폰 번호</option>
+                        </select>
+                        <input type="text" name="keyword" id="keyword" class="input-search"
+                            autocomplete="on" placeholder="검색어를 입력하세요">
+                        <a href="javascript:;" class="dm-btn" id="btn-search">검색</a>
+                    </div>
+                </div>
 
                 <!--결제관리 table-->
                 <table id="m-payment-table">
@@ -28,6 +34,7 @@
                         <th>핸드폰 번호</th>
                         <th>요청사항</th>
                         <th>총 가격</th>
+                        <th>배송상태</th>
                     </tr>
                     <tr class="m-payment-table-content">
                         <td id="m-payment-date">2024-01-01</td>
@@ -36,6 +43,7 @@
                         <td id="m-payment-hp">010-1234-5678</td>
                         <td id="m-payment-request">빠른 배송 부탁드립니다.</td>
                         <td id="m-payment-total-price">10,000원</td>
+                        <td>배송완료</td>
                     </tr>
                     <tr class="m-payment-table-content">
                         <td id="m-payment-date">2024-01-01</td>
@@ -44,6 +52,7 @@
                         <td id="m-payment-hp">010-1234-5678</td>
                         <td id="m-payment-request">빠른 배송 부탁드립니다.</td>
                         <td id="m-payment-total-price">10,000원</td>
+                        <td>배송완료</td>
                     </tr>
                     <tr class="m-payment-table-content">
                         <td id="m-payment-date">2024-01-01</td>
@@ -52,8 +61,21 @@
                         <td id="m-payment-hp">010-1234-5678</td>
                         <td id="m-payment-request">빠른 배송 부탁드립니다.</td>
                         <td id="m-payment-total-price">10,000원</td>
+                        <td>배송완료</td>
                     </tr>
                 </table>
+
+                <!-- 페이지네이션 -->
+                <div class="pagination">
+                    <a href="#" class="page-arrow" id="first">&laquo;</a>
+                    <a href="#" class="page-arrow" id="prev">&lsaquo;</a>
+                    <!-- 페이지 번호 목록 -->
+                    <div class="page-list">
+
+                    </div>
+                    <a href="javascript:;" class="page-arrow" id="next">&rsaquo;</a>
+                    <a href="javascript:;" class="page-arrow" id="last">&raquo;</a>
+                </div>
             </div>
             <!-- //greetings -->
             <div class="clear"></div>
