1eea171c16144c816d11b5bfac19239a019ee07f
poula9744 <poula9744@naver.com>
2024-04-15 21:06:34 +0900

    이은빈

--- src/views/customer/PaymentPage/PaymentPageView.vue
+++ src/views/customer/PaymentPage/PaymentPageView.vue
@@ -1,160 +1,168 @@
 <template>
-<div>
-    <AppHeader />
-    <div id="wrap">
-      <div id="container" class="clearfix">
-        <!-- aside 없음 -->
-        <div id="full-content">
-          <!-- content-head 없음 -->
-
-          <div id="index">
-            <div id="greetings">
-                <h1>결제/주문 창</h1>
-                <form action="" method="post">
-
-                    <!-- 상품 정보 영역-->
-                    <div class="u-payment-product-information">
-                        <div class="u-payment-title">상품 정보</div>
-                        <p>결제자: 유영수</p>
-                        <p>영수 킹갓제너럴스페셜 티셔츠</p>
-                        <p>핑크 (S)</p>
-                        <p>수량: 2개</p>
-
-                        <!-- 상품 가격표시 영역 -->
-                        <div id="price">200,000원</div>
-                    </div>
-
-                    <div class="u-payment-product-information">
-                        <div class="u-payment-title">상품 정보</div>
-                        <p>결제자: 유영수</p>
-                        <p>영수 킹갓제너럴스페셜 진</p>
-                        <p>노랑 (S)</p>
-                        <p>수량: 1개</p>
-
-                        <!-- 상품 가격표시 영역 -->
-                        <div id="price">100,000원</div>
-                    </div>
-
-                    <!--받는 분 연락처-->
-                    <div id="u-payment-phone">
-                        <label for="phone">연락처 입력:</label>
-                        <input type="tel" name="phone" id="u-payment-phone-number" placeholder="연락처">
-                    </div>
-
-                    <!--배송지 입력-->
-                    <div id="u-payment-delivery-address">
-                        <p id="u-payment-delivery-address-title">배송지 주소:</p>
-
-                        <!-- 주문자와 동일 -->
-                        <p>경기도 하남시 어쩌고 저쩌고</p>
-
-                        <!-- 신규 배송지 -->
-                        <div id="u-payment-delivery-new-address">
-                            <label for="address">배송지 입력:</label>
-                            <input type="text" name="address" id="u-payment-new-address" placeholder="주소">
-                            <button>완료</button>
+    <div>
+        <AppHeader />
+        <div id="wrap">
+          <div id="container" class="clearfix">
+            <!-- aside 없음 -->
+            <div id="full-content">
+              <!-- content-head 없음 -->
+    
+              <div id="index" class="u-payment-all-all">
+                <div id="greetings" class="u-payment-all">
+                    <h1 id="u-payment-h1">결제/주문 창</h1>
+                    <form action="" method="post">
+    
+                        <!-- 상품 정보 영역-->
+                        <div class="u-payment-product-information">
+                            <div class="u-payment-title">상품 정보</div>
+                            <p id="u-payment-product-name">영수 킹갓제너럴스페셜 티셔츠</p>
+                            <p id="u-payment-product-intro">영수 대박대박 형광핑크 형광핑크 (S)</p>
+                            <p id="u-payment-product-quantity">수량: 2개</p>
+                            
+                            <!-- 상품 가격표시 영역 -->
+                            <dir id="u-payment-product-qqqprice">
+                                <p id="u-payment-product-qqq">개당: 100,000원</p>
+                                <div id="u-payment-product-qprice">총 200,000원</div>
+                            </dir>   
+                        </div>
+    
+                        <!-- 상품 정보 영역-->
+                        <div class="u-payment-product-information">
+                            <div class="u-payment-title">상품 정보</div>
+                            <p id="u-payment-product-name">영수 킹갓제너럴스페셜 진</p>
+                            <p id="u-payment-product-intro">영수 대박대박 형광노랑 형광노랑 (S)</p>
+                            <p id="u-payment-product-quantity">수량: 1개</p>
+                            
+                            <!-- 상품 가격표시 영역 -->
+                            <dir id="u-payment-product-qqqprice">
+                                <p id="u-payment-product-qqq">개당: 100,000원</p>
+                                <div id="u-payment-product-qprice">총 100,000원</div>
+                            </dir>   
+                        </div>
+    
+    
+                        <!--받는 분 연락처-->
+                        <div id="u-payment-phone">
+                            <label for="u-payment-phone-number">연락처 입력:</label>
+                            <input type="tel" name="phone" id="u-payment-phone-number" placeholder="연락처">
+                        </div>
+    
+    
+                        <!--배송지 입력-->
+                        <div id="u-payment-delivery-address">
+                            
+    
+                            <!-- 주문자와 동일 -->
+                            <div id="u-payment-delivery-org-address">
+                                <p id="u-payment-delivery-address-title">기존 배송지 주소:</p>
+                                <p>경기도 하남시 어쩌고 저쩌고</p>
+                            </div>
+    
+                            <!-- 신규 배송지 -->
+                            <div id="u-payment-delivery-new-address">
+                                <label for="u-payment-new-address">신규 배송지 입력:</label>
+                                <input type="text" name="address" id="u-payment-new-address" placeholder="주소">
+                                <button>완료</button>
+                            </div>
                         </div>
                         <!--요청사항-->
                         <div class="u-payment-request-class">
-                            <label for="request">요청사항</label>
-                            <textarea id="request" placeholder="여기에 입력해주세요" maxlength="500"></textarea>
+                            <label for="u-payment-request">요청사항</label>
+                            <textarea id="u-payment-request" placeholder="여기에 입력해주세요" maxlength="500"></textarea>
                         </div>
-                    </div>
-
-                    <!-- 포인트 -->
-                    <div id="u-payment-point">
-                        <p>현재 포인트: 500원</p>
-                        <label for="point">포인트: </label>
-                        <input type="text" name="point" id="u-payment-point-push" placeholder="0">
-                        <button id="u-payment-point-button">사용</button>
-                    </div>
-
-                    <!--총 결제 금액-->
-                    <div class="u-payment-total-price-all">
-                        <div class="u-payment-total-price">
-                            <table class="table_price">
-                                <tbody>
-                                    <tr>
-                                        <th>상품금액</th>
-                                        <td><span>영수 킹갓제너럴스페셜 티셔츠</span>200,000<span>원</span></td>
-                                        <td><span>영수 킹갓제너럴스페셜 진</span>100,000<span>원</span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                        
+    
+                        <!-- 포인트 -->
+                        <div id="u-payment-point">
+                            <p>현재 포인트: 500원</p>
+                            <label for="point">포인트: </label>
+                            <input type="text" name="point" id="u-payment-point-push" placeholder="0">
+                            <button id="u-payment-point-button">사용</button>
                         </div>
-                        <div class="u-payment-total-price">
-                            <table class="table_price">
-                                <tbody>
-                                    <tr>
-                                        <th>포인트 사용금액</th>
-                                        <td>0<span>원</span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
+    
+                        <!--총 결제 금액-->
+                        <div class="u-payment-total-price-all">
+                            <div class="u-payment-total-price">
+                                <table class="u-table_price">
+                                    <tbody>
+                                        <tr id="u-payment-product">
+                                            <th id="u-payment-product-price">상품금액</th>
+                                            <td><span>영수 킹갓제너럴스페셜 티셔츠</span>200,000<span>원</span></td>
+                                            <td><span>영수 킹갓제너럴스페셜 진</span>100,000<span>원</span></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="u-payment-total-price">
+                                <table class="table_price">
+                                    <tbody>
+                                        <tr>
+                                            <th>포인트 사용금액</th>
+                                            <td>0<span>원</span></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="u-payment-total-price">
+                                <table class="u-table_price">
+                                    <tbody>
+                                        <tr>
+                                            <th>총금액</th>
+                                            <td id="u-total-price">300,000<span>원</span></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                        <div class="u-payment-total-price">
-                            <table class="table_price">
-                                <tbody>
-                                    <tr>
-                                        <th>총금액</th>
-                                        <td id="u-total-price">300,000<span>원</span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                        
+    
+                        <!-- 결제 방법 영역-->
+                        <div class="u-payment-method-button">
+                            <label for="payment-method">결제 수단 선택:</label>
+                            <button type="button" id="payment-method-button">신용카드</button>
+                            <button type="button" id="bank-transfer-button">계좌이체</button>
+                            <button type="button" id="paypal-button">네이버페이</button>
                         </div>
-                    </div>
-                    
-                    <div >
-                        <label for="u-total-price">총금액</label>
-                        <div >300,000원</div>
-                    </div>
-
-                    <!-- 결제 방법 영역-->
-                    <div class="u-payment-method-button">
-                        <label for="payment-method">결제 수단 선택:</label>
-                        <button type="button" id="payment-method-button">신용카드</button>
-                        <button type="button" id="bank-transfer-button">계좌이체</button>
-                        <button type="button" id="paypal-button">페이팔</button>
-                    </div>
-                    
-                    <!-- 결제 버튼 -->
-                    <button id="payment-final-button">결제하기</button>
-                </form>
+                        
+                        <!-- 결제 버튼 -->
+                        <button id="payment-final-button">결제하기</button>
+                    </form>
+                </div>
+                <!-- //greetings -->
+                <div class="clear"></div>
+              </div>
+              <!-- //index -->
             </div>
-            <!-- //greetings -->
-            <div class="clear"></div>
+            <!-- //full-content -->
           </div>
-          <!-- //index -->
+          <!-- //container -->
+    
+          <!-- //footer -->
         </div>
-        <!-- //full-content -->
-      </div>
-      <!-- //container -->
-
-      <!-- //footer -->
-    </div>
-    <!-- //wrap -->
-    <AppFooter />
-</div>   
-</template>
-<script>
-import "@/assets/css/customer/PaymentPageView.css";
-import AppFooter from "@/components/customer/AppFooter.vue";
-import AppHeader from "@/components/customer/AppHeader.vue";
-
-export default {
-    name: "PaymenyPageView",
-    components: {
-        AppFooter,
-        AppHeader
-    },
-   data() {
-        return {};
-    },
-    methods: {},
-    created() { }
-};
-</script>
-<style>
-
-
-</style>
+        <!-- //wrap -->
+        <AppFooter />
+    </div>   
+    </template>
+    <script>
+    import "@/assets/css/customer/PaymentPageView.css";
+    import AppFooter from "@/components/customer/AppFooter.vue";
+    import AppHeader from "@/components/customer/AppHeader.vue";
+    
+    export default {
+        name: "PaymenyPageView",
+        components: {
+            AppFooter,
+            AppHeader
+        },
+       data() {
+            return {};
+        },
+        methods: {},
+        created() { }
+    };
+    </script>
+    <style>
+    
+    
+    </style>
+    
