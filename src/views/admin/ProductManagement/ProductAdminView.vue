60f48758f340a496806b25f0f49fed2f0d7cca91
kodasom0302 <dasom589@gmail.com>
2024-04-17 15:50:44 +0900

    고다솜

--- src/views/admin/ProductManagement/ProductAdminView.vue
+++ src/views/admin/ProductManagement/ProductAdminView.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <AdminAppHeader />
+        <AppHeader />
         <div id="wrap">
             <div id="container" class="clearfix">
                 <!-- aside 없음 -->
@@ -16,31 +16,6 @@
 
                             <div id="productFormGroup">
 
-                                <!--
-                                <ul>
-                                    <li><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></li>
-                                    <li>영수 스페셜 원피스</li>
-                                    <li><strong>10,000원</strong></li>
-                                    <li><router-link to="">수정</router-link></li>
-                                    <li><router-link to="">삭제</router-link></li>
-                                </ul>
-
-                                <ul>
-                                    <li><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></li>
-                                    <li>영수 스페셜 진</li>
-                                    <li><strong>8,000원</strong></li>
-                                    <li><router-link to="">수정</router-link></li>
-                                    <li><router-link to="">삭제</router-link></li>
-                                </ul>
-
-                                <ul>
-                                    <li><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></li>
-                                    <li>에스파 닝수 착용!!! 프린세스 원피스</li>
-                                    <li><strong>1,000,000원</strong></li>
-                                    <li><router-link to="">수정</router-link></li>
-                                    <li><router-link to="">삭제</router-link></li>
-                                </ul>
-                                -->
 
                                 <table>
                                     <thead>
@@ -52,47 +27,17 @@
                                         </tr>
                                     </thead>
 
-                                    <tbody>
+                                    <tbody v-bind:key="i" v-for="(pVo, i) in pList">
                                         <tr>
-                                            <td><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></td>
-                                            <td>영수 스페셜 티셔츠</td>
-                                            <td>10,000원</td>
-                                            <td><router-link id="btnDelete" to="">삭제</router-link></td>
-                                        </tr>
-
-                                        <tr>
-                                            <td><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></td>
-                                            <td>영수 스페셜 진</td>
-                                            <td>8,000원</td>
-                                            <td><router-link id="btnDelete" to="">삭제</router-link></td>
-                                        </tr>
-
-                                        <tr>
-                                            <td><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></td>
-                                            <td>에스파 닝수 착용!!! 프린세스 원피스</td>
-                                            <td>1,000,000원</td>
-                                            <td><router-link id="btnDelete" to="">삭제</router-link></td>
-                                        </tr>
-
-                                        <tr>
-                                            <td><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></td>
-                                            <td>영수 스페셜 티셔츠</td>
-                                            <td>10,000원</td>
-                                            <td><router-link id="btnDelete" to="">삭제</router-link></td>
-                                        </tr>
-
-                                        <tr>
-                                            <td><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></td>
-                                            <td>영수 스페셜 진</td>
-                                            <td>8,000원</td>
-                                            <td><router-link id="btnDelete" to="">삭제</router-link></td>
-                                        </tr>
-
-                                        <tr>
-                                            <td><img id="clothesImage" src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg"></td>
-                                            <td>에스파 닝수 착용!!! 프린세스 원피스</td>
-                                            <td>1,000,000원</td>
-                                            <td><router-link id="btnDelete" to="">삭제</router-link></td>
+                                            <td>
+                                                <img id="clothesImage"
+                                                    src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg">
+                                            </td>
+                                            <td>{{ pVo.p_name }}</td>
+                                            <td>{{ pVo.p_price }}원</td>
+                                            <td>
+                                                <button v-on:click="remove(pVo.p_no)">삭제</button>
+                                            </td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -116,26 +61,83 @@
             <!-- //footer -->
         </div>
         <!-- //wrap -->
-        <AdminAppFooter />
+        <AppFooter />
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import "@/assets/css/admin/ProductAdminView.css";
-import AdminAppFooter from "@/components/customer/AdminAppFooter.vue";
-import AdminAppHeader from "@/components/customer/AdminAppHeader.vue";
+import AppFooter from "@/components/customer/AppFooter.vue";
+import AppHeader from "@/components/customer/AppHeader.vue";
 
 export default {
     name: "ProductAdminView",
     components: {
-        AdminAppHeader,
-        AdminAppFooter,
+        AppHeader,
+        AppFooter,
     },
     data() {
-        return {};
+        return {
+            pList: [],
+            pVo: {
+                p_no: "",
+                p_name: "",
+                p_price: ""
+            },
+        };
     },
-    methods: {},
-    created(){},
+    methods: {
+        getList() {
+            console.log("리스트 가져오기");
+
+            axios({
+                method: "get", // put, post, delete
+                url: "http://localhost:9002/api/admin/productlist",
+                headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+                //params: guestbookVo, //get방식 파라미터로 값이 전달
+                //data: guestbookVo, //put, post, de    lete 방식 자동으로 JSON으로 변환 전달
+
+                responseType: "json", //수신타입
+            })
+                .then((response) => {
+                    //console.log(response.data.apiData); //수신데이타
+                    this.pList = response.data.apiData;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+        },
+        remove(p_no){
+            console.log("클릭클릭");
+            console.log(p_no);
+
+            axios({
+                method: 'delete', // put, post, delete                   
+                url: 'http://localhost:9002/api/admin/delete/' + this.p_no,
+                headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
+                //params: guestbookVo, //get방식 파라미터로 값이 전달
+                data: { p_no }, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+                responseType: 'json' //수신타입
+            }).then(response => {
+                console.log(response.data); //수신데이타
+
+                if (response.data.count > 0) {
+                    this.pList.splice(response.data, 1);
+                } else {
+                    alert("삭제에 실패했습니다");
+                }
+
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+    },
+    created() {
+        this.getList();
+    },
 };
 </script>
 
