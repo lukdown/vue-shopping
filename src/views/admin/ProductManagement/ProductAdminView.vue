729b04e0b02afcc49e98958dcdb38635cd7e30d6
pjh990909 <pjh990909@naver.com>
2024-04-18 17:50:10 +0900

    qwg

--- src/views/admin/ProductManagement/ProductAdminView.vue
+++ src/views/admin/ProductManagement/ProductAdminView.vue
@@ -16,7 +16,7 @@
 
                             <div id="productFormGroup">
 
-
+                                <router-link id="addBtn" to="/admin/productadd">등록</router-link>
                                 <table>
                                     <thead>
                                         <tr>
@@ -30,22 +30,32 @@
                                     <tbody v-bind:key="i" v-for="(pVo, i) in pList">
                                         <tr id="trOne">
                                             <td>
-                                                <!--
-                                                    <img id="clothesImage"
-                                                    src="https://www.fashionn.com/files/board/2018/image/p1cforbgmvcok1ii51o8c1tecoc11.jpg">
-                                                -->
-                                                <img id="clothesImage" v-bind:src="`http://localhost:9002/upload/${pVo.saveName}`">
+                                                <img id="clothesImage"
+                                                    v-bind:src="`http://localhost:9002/upload/${pVo.saveName}`">
                                             </td>
                                             <td>{{ pVo.p_name }}</td>
                                             <td>{{ pVo.p_price }}원</td>
                                             <td>
-                                                <button v-on:click="remove(pVo.p_no)">삭제</button>
+                                                <button v-if="pVo.ep_no == 0" v-on:click="remove(pVo.p_no)" type="button">삭제</button>
                                             </td>
                                         </tr>
                                     </tbody>
                                 </table>
+                                <input type="text" id="productadminSearch" placeholder="검색" v-model="pVo.keyword"
+                                    v-on:keyup.enter="search">
+                                <ol id="productadminList">
 
-                                <router-link id="addBtn" to="">등록</router-link>
+                                    <li class="" v-if="prev != false" v-on:click="prevPage">이전</li>
+                                    <li class="" v-else-if="prev == true" v-on:click="prevPage">이전</li>
+                                    <li id="productadminlistpage" v-bind:key="index" v-for="(i, index) in endNo - startNo + 1">
+
+                                        <a v-on:click.prevent="list(startNo + i)">{{ startNo + i - 1 }}</a>
+
+                                    </li>
+                                    <li v-if="next == true" v-on:click="nextPage">다음</li>
+
+                                </ol>
+                                
 
                             </div>
 
@@ -83,42 +93,77 @@ export default {
     data() {
         return {
             pList: [],
-            pVo: {
-                p_no: "",
-                p_name: "",
-                p_price: "",
-                saveName: ""
+            pVo: { 
+                crtPage: 1,
+                keyword: ""
             },
+            startNo: 0,
+            endNo: 0,
+            next: "",
+            prev: ""
         };
     },
     methods: {
-        getList() {
+        getList(list) {
             console.log("리스트 가져오기");
+            if (this.pVo.crtPage == 1) {
+                this.pVo.crtPage = 1;
+            } else if (this.pVo.crtPage < 1) {
+                this.pVo.crtPage = 1;
+            }
+            else {
+                this.pVo.crtPage = list - 1;
+            }
 
+            console.log(this.pVo.crtPage);
             axios({
-                method: "get", // put, post, delete
+                method: "post", // put, post, delete
                 url: "http://localhost:9002/api/admin/productlist",
                 headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
                 //params: guestbookVo, //get방식 파라미터로 값이 전달
-                //data: guestbookVo, //put, post, de    lete 방식 자동으로 JSON으로 변환 전달
+                data: this.pVo, //put, post, de    lete 방식 자동으로 JSON으로 변환 전달
 
                 responseType: "json", //수신타입
             })
                 .then((response) => {
-                    //console.log(response.data.apiData); //수신데이타
-                    this.pList = response.data.apiData;
+                    console.log(response.data.apiData); //수신데이타
+                    this.pList = response.data.apiData.pList;
+                    this.endNo = response.data.apiData.endPageBtnNo;
+                    this.startNo = response.data.apiData.startPageBtnNo;
+                    this.next = response.data.apiData.next;
+                    this.prev = response.data.apiData.prev
                 })
                 .catch((error) => {
                     console.log(error);
                 });
 
-        },/*
+        },
+        search() {
+            this.pVo.crtPage = 1;
+            this.getList();
+        },
+        list(page) {
+            this.pVo.crtPage = page;
+            this.getList(this.pVo.crtPage);
+        },
+        prevPage() {
+            if (this.prev == false) {
+                console.log(this.pVo.crtPage);
+                this.getList(this.pVo.crtPage);
+            }
+        },
+        nextPage() {
+            if (this.next == true) {
+                this.pVo.crtPage = this.pVo.crtPage + 6;
+                this.getList(this.pVo.crtPage);
+            }
+        },
         remove(p_no){
             console.log("클릭클릭");
             console.log(p_no);
 
             axios({
-                method: 'post', // put, post, delete                   
+                method: 'delete', // put, post, delete                   
                 url: 'http://localhost:9002/api/admin/delete/'+p_no,
                 headers: { "Content-Type": "application/json; charset=utf-8" }, //전송타입
                 //params: guestbookVo, //get방식 파라미터로 값이 전달
@@ -126,19 +171,14 @@ export default {
 
                 responseType: 'json' //수신타입
             }).then(response => {
-                console.log(response.data); //수신데이타
+                console.log(response.data.apiData); //수신데이타
 
-                const row=document.getElementsByTagName('tr');
-                document.getElementById('trOne').style.display
-
-                row.style.display='none';
-
-                this.getLst();
+                this.getList();
 
             }).catch(error => {
                 console.log(error);
             });
-        }*/
+        }
     },
     created() {
         this.getList();
