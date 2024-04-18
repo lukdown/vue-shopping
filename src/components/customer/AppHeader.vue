c6fee7113eb3f4d517cd5c89372708fbcc4de84a
Ksusie0214 <tdlrtmvldj@gmail.com>
2024-04-18 12:20:08 +0900

    김수빈

--- src/components/customer/AppHeader.vue
+++ src/components/customer/AppHeader.vue
@@ -9,7 +9,7 @@
                     </button>
 
                     <router-link to="/">
-                        <img id="logo" src="@/assets/img/logo.png" alt="youngground" />
+                        <img id="logo-img" src="@/assets/img/logo.png" alt="youngground" />
                     </router-link>
 
                     <div id="ls-gather-btn-box">
